7c0a0f682ce8d891a43d56e9268e4764901c0d43
Daniel Rodriguez <danielrodriguez@Daniels-MBP.domain>
2016-10-12 07:22:09 -0600

    sort finally working

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,75 +1,102 @@
 require_relative 'node'
 
 class BinarySearchTree
-    attr_reader :root
+  attr_reader :root
 
-    def insert(score, title)
-        if root == nil
+  def insert(score, title)
+    if root == nil
             @root = Node.new(score,title)
             return 1
         else
             place_node(root, score, title)
         end
     end
+    
     def place_node(root, score, title)
     ##Must account for == case
-    depth = 2
-    if score < root.score
-      if root.left_child == nil
-      root.left_child = Node.new(score, title)
-      
-      else
-        depth += 1
-        place_node(root.left_child, score, title)
-      end
-    else
-      if root.right_child == nil
-        root.right_child = Node.new(score, title)
-      else
-        depth +=1
-        place_node(root.right_child, score, title)
-      end
-    end
+        depth = 2
+        if score < root.score
+            if root.left_child == nil
+                root.left_child = Node.new(score, title)
+            else
+                depth += 1
+                place_node(root.left_child, score, title)
+            end
+        else
+            if root.right_child == nil
+                root.right_child = Node.new(score, title)
+            else
+                depth +=1
+                place_node(root.right_child, score, title)
+            end
+        end
     return depth
   end
-  def include(score)
+  
+  def include?(score)
     match = search(score)
-    if match != nil
+    if match
       return true
     else
       return false
     end
   end
+  
   def search(score)
-    current = @root
-      until current == nil || current.score == score
+    current = root
+    until current == nil || current.score == score
         if score < current.score
-          current = current.left_child
+            current = current.left_child
         else
-          current = current.right_child
+            current = current.right_child
         end
      end
      return current 
   end
-  #Reports the depth of the tree where a score appears. 
-  #Return nil if the score does not exist
+  
   def depth_of(score)
+    current = root
+    depth = 1
+    if search(score) == nil
+        return nil
+    else
+        until current.score == score
+            if score < current.score
+                depth += 1
+                current = current.left_child
+            else
+                depth +=1
+                current = current.right_child
+            end
+        end
+        return depth
+    end
   end
 
   def max
-    highest = @root
-    until highest.right_child == nil
+    highest = root
+    if highest.right_child
       highest = highest.right_child
     end
     return highest
   end
 
   def min
-    lowest = @root
-    until lowest.left_child == nil
+    lowest = root
+    if lowest.left_child
       lowest = lowest.left_child
     end
     return lowest
+  end
+  def sort(node)
+    if node.left_child
+      sort(node.left_child) 
+    end
+    p node.score
+    if node.right_child
+      sort(node.right_child)
+    end
+
   end
 end
 
--- date_night.rb
+++ date_night.rb
@@ -1,8 +1,26 @@
 require_relative 'binary_search_tree'
 require_relative 'node'
+require 'rubygems'
+
+file = File.new('movies.txt', 'r')
+file.each do |line|
+    puts line.split(',' ,  2)
+end
 
 tree = BinarySearchTree.new
-p tree.insert(61, "Bill & Ted's Excellent Adventure")
-p tree.insert(16, "Johnny English")
-p tree.insert(92, "Sharknado 3")
-p tree.insert(50, "Hannibal Buress: Animal Furnace")
+tree.insert(61, "Bill & Ted's Excellent Adventure")
+tree.insert(16, "Johnny English")
+tree.insert(92, "Sharknado 3")
+tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+tree.include?(16)
+tree.include?(65)
+
+p tree.max
+p tree.min
+
+tree.depth_of(85)
+tree.depth_of(16)
+tree.depth_of(50)
+
+tree.sort(tree.root)
